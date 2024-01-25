bd1577fdd9fa8928557a73c3db445c9aa6051c2b
ANGELA-PARGA <angela.parga.g@gmail.com>
2024-01-25 16:36:21 -0500

    rendering videos, links and thumbnail images in PostCard component, adding CSS styles

--- src/components/post_cards/PostCard.js
+++ src/components/post_cards/PostCard.js
@@ -3,14 +3,21 @@ import styles from './PostCard.module.css'
 import { UilCommentChartLine } from '@iconscout/react-unicons'
 import { UilAnalysis } from '@iconscout/react-unicons'
 import ReactMarkdown from 'react-markdown'
+import { useState } from 'react'
 
 
 export function PostCard({data, fullText}){
+    const [load, setLoad] = useState(true)
+    const media = data.secure_media && data.secure_media.reddit_video?.fallback_url;
 
     function isValidImageURL(url) {
         const imageExtensions = ['.png', '.jpg', '.jpeg', '.gif'];
         const lowercasedURL = url.toLowerCase();
         return imageExtensions.some(ext => lowercasedURL.endsWith(ext));
+    }
+
+    function handleOnError(){
+        setLoad(false)
     }
 
     return (
@@ -21,10 +28,30 @@ export function PostCard({data, fullText}){
                 <p><span>Posted by:</span> u/{data.author}</p>                
             </div>
             <h2>{data.title}</h2>
-            {data.url && isValidImageURL(data.url) ? <div className={styles.thumbnail}>
-                <img src={data.url} alt='thumbnail'></img> 
-            </div> :<></>}
-            <div className={styles.text}><ReactMarkdown>{data.selftext}</ReactMarkdown></div>
+            { data.url && isValidImageURL(data.url) ? 
+                <div className={styles.thumbnail}>
+                    <img src={data.url} alt='embded'></img> 
+                </div> : 
+                <div className={styles.embdedInfo}>
+                    {!data.thumbnail ? <></> : 
+                    <img src={data.thumbnail} 
+                        alt="thumbnail" 
+                        onError={handleOnError}  
+                        style={{ display: load ? 'block' : 'none' }}
+                    />}
+                    {!load && media ?  
+                    <video src={media} controls className={styles.videoPlayer}></video> :
+                    <></>
+                    }
+                    <a href={data.url_overridden_by_dest} 
+                        target="_blank" 
+                        rel="noopener noreferrer">{data.url_overridden_by_dest}
+                    </a>
+                </div> 
+            }
+            <div className={styles.text}>
+                <ReactMarkdown>{data.selftext}</ReactMarkdown>
+            </div>
             <div className={styles.cardInfo}>
                 <div>
                     <UilCommentChartLine size={20} color='#D42B2B'/>
